817267727bbf02df9a112c36d8d6c2eb5558e24b
Derek Adams <derek.adams@sitewhere.com>
2019-04-21 11:02:42 -0400

    Change types to 'any'.

--- types/sitewhere.d.ts
+++ types/sitewhere.d.ts
@@ -1,38 +1,36 @@
-import Vue from "vue";
-
 // Common components.
-export const ClipboardCopyField: Vue;
-export const ColorInputField: Vue;
-export const ColorPicker: Vue;
-export const DateTimePicker: Vue;
-export const ErrorBanner: Vue;
-export const FloatingActionButton: Vue;
-export const HeaderField: Vue;
-export const IconSelector: Vue;
-export const LinkedHeaderField: Vue;
-export const LoadingOverlay: Vue;
+export const ClipboardCopyField: any;
+export const ColorInputField: any;
+export const ColorPicker: any;
+export const DateTimePicker: any;
+export const ErrorBanner: any;
+export const FloatingActionButton: any;
+export const HeaderField: any;
+export const IconSelector: any;
+export const LinkedHeaderField: any;
+export const LoadingOverlay: any;
 
 // Dialog components.
-export const BaseDialog: Vue;
-export const ConfirmDialog: Vue;
-export const DeleteDialog: Vue;
-export const MetadataPanel: Vue;
+export const BaseDialog: any;
+export const ConfirmDialog: any;
+export const DeleteDialog: any;
+export const MetadataPanel: any;
 
 // List components.
-export const ListEntry: Vue;
-export const ListLayout: Vue;
-export const ListPage: Vue;
-export const ListTab: Vue;
-export const Pager: Vue;
+export const ListEntry: any;
+export const ListLayout: any;
+export const ListPage: any;
+export const ListTab: any;
+export const Pager: any;
 
 // Navigation components.
-export const ContentTab: Vue;
-export const DataEntryPanel: Vue;
-export const DataTableTab: Vue;
-export const DetailPage: Vue;
-export const InAppFooter: Vue;
-export const InAppSystemBar: Vue;
-export const Navigation: Vue;
-export const NavigationActionButton: Vue;
-export const NavigationHeaderPanel: Vue;
-export const NavigationPage: Vue;
+export const ContentTab: any;
+export const DataEntryPanel: any;
+export const DataTableTab: any;
+export const DetailPage: any;
+export const InAppFooter: any;
+export const InAppSystemBar: any;
+export const Navigation: any;
+export const NavigationActionButton: any;
+export const NavigationHeaderPanel: any;
+export const NavigationPage: any;
