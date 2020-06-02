b7457d38566f120c6877c5eadc7ac921a6afdc75
Derek Adams <derek.adams@sitewhere.com>
2020-06-02 16:07:20 -0400

    fix: Add missing Vuetify declaration.

--- src/components/configuration/NewElementEntry.vue
+++ src/components/configuration/NewElementEntry.vue
@@ -14,10 +14,10 @@
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 
-import { VCard, VCardText, VIcon } from "vuetify/lib";
+import { VCard, VCardText, VIcon, VDivider } from "vuetify/lib";
 
 @Component({
-  components: { VCard, VCardText, VIcon }
+  components: { VCard, VCardText, VIcon, VDivider }
 })
 export default class NewElementChooser extends Vue {
   @Prop() readonly icon!: string;
